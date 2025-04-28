4585b6decabfa87c977682289669bfe28a3847a2
Stephen <b00122401@mytudublin.ie>
2025-04-28 22:33:13 +0100

    refactored the function and made it shorter

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-def main():
-    ans = input("Please enter a Word: ").lower()
-    l = 0
-    r = len(ans)-1
-    
-    while l < r:
-        if word[l] != word[r]:
-            return f'{ans} is Not a Palindrome'
-        l+=1
-        r-=1
-    return f'{ans} is a Palindrome'
-
-    
-
-
 if __name__ == "__main__":
-    print(main())
+    ans = input("Please enter a Word: ").lower()
+    
+    if ans == ans[::-1]:
+        print(ans, 'is a palindrome')
+    else:
+        print(ans,'is not a palindrome')
